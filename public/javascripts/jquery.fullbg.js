d2495350411219650144b8574b343bb93d3c1eac
mike <mike@god.(none)>
2011-02-03 19:28:13 +0200

    optimized resizing

--- public/javascripts/jquery.fullbg.js
+++ public/javascripts/jquery.fullbg.js
@@ -6,52 +6,86 @@
 **/
 
 (function($) {
-  $.fn.fullBg = function(){
+  $.fn.fullBg = function(options){
+    var defaults = {
+      animated: true
+    }
+    options = $.extend(defaults, options);
     var bgImg = $(this);		
     var original_img = {
       width: bgImg.width(),
       height: bgImg.height()
     }
     var img_ratio = original_img.width / original_img.height;
-    var img,win,ratio,diff;
- 
-    function resizeImg() {
-      img = {
+    bgImg.data("img_ratio", img_ratio);
+    bgImg_count = $(window).data("bgImg_count")*1;
+    if(!bgImg_count) {bgImg_count = 0}
+    $(window).data("bgImg_count", bgImg_count+1);
+    var uid = "bgImg_"+(bgImg_count+1);
+    bgImg.attr("id", uid);
+    
+    $.fn.fitToWindow = function() {
+      bgImg = $(this);
+      var img = {
         width: bgImg.width(),
         height: bgImg.height()
       }
-      win = {
+      var win = {
         width: $(window).width(),
         height: $(window).height()
       }
-      ratio = {
+      var ratio = {
         width: (win.width / img.width),
         height: (win.height / img.height)
       }
-      diff = {
+      var diff = {
         width: (ratio.height * img.width),
         height: (ratio.width * img.height)
       }
-      if((diff.height > win.height) && img_ratio > 1) {
+      if((diff.height > win.height) && bgImg.data("img_ratio") > 1) {
         bgImg.css({
           width: win.width + 'px',
-          height: '',
-          left: -(img.width-win.width)/2+'px',
-          top: -(img.height-win.height)/2+'px'
+          height: ''
         });
       } else {
         bgImg.css({
           height: win.height + 'px',
-          width: '',
-          left: -(img.width-win.width)/2+'px',
-          top: -(img.height-win.height)/2+'px'
+          width: ''
         });		
       }
+      bgImg.css({
+        left: -(bgImg.width()-win.width)/2+'px',
+        top: -(bgImg.height()-win.height)/2+'px'
+      });
     } 
-    $(window).resize(function() {
-      resizeImg();
-    }); 
-    resizeImg();
-    $(window).resize();
+
+    if(options.animated) {
+      $(window).resize(function() {
+        $("#"+uid).fitToWindow();
+      }); 
+    } else {
+      var rtime = new Date(1, 1, 2000, 12,00,00);
+      var timeout = false;
+      var delta = 200;
+      
+      $(window).resize(function() {
+        rtime = new Date();
+        if (timeout === false) {
+          timeout = true;
+          setTimeout(resizeEnd, delta);
+        }
+      });
+
+      function resizeEnd() {
+       if(new Date() - rtime < delta) {
+         setTimeout(resizeEnd, delta);
+       } else {
+         timeout = false;
+         $("#"+uid).fitToWindow();
+        }    
+      }
+
+    }
+    bgImg.fitToWindow();
   };
 })(jQuery)
